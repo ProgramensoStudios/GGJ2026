31e5469778a34a03f636dc710dca640f1b1b9e5e
Kary <kary.velle@gmail.com>
2026-01-31 02:17:43 -0600

    se mueve la capsula

---
+++ LittleMensos/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Vector2 moveInput;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
